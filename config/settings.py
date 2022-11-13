8863681010d2fc949738dafc517cb36de7c5faaf
ahti1405 <aktan85@mail.ru>
2022-11-13 18:59:20 +0600

    Blog API setup

--- config/settings.py
+++ config/settings.py
@@ -37,17 +37,31 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third Party
+    'rest_framework',
+    'corsheaders',
+    # Local
+    'accounts.apps.AccountsConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+# corsheaders config
+CORS_ORIGIN_WHITELIST = {
+    'http://localhost:3000',
+    'http://localhost:8000',
+}
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
+# end of corsheaders config
 
 ROOT_URLCONF = "config.urls"
 
@@ -121,3 +135,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# restframework config
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
